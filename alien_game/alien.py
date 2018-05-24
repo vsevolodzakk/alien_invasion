338e9300e5cf7986786a43581e799a31852e626f
Vsevolod <30939590+vsevolodzakk@users.noreply.github.com>
2018-05-25 01:21:11 +0300

    Add files via upload

--- alien_game/alien.py
+++ alien_game/alien.py
@@ -8,17 +8,26 @@ class Alien(Sprite):
 		super(Alien, self).__init__()
 		self.screen = screen
 		self.game_settings = game_settings
-		
+
 		#Loading alien image
 		self.image = pygame.image.load('images/invader.png')
 		self.rect = self.image.get_rect()
-		
+
 		#Every alien will appear in the top left corner
 		self.rect.x = self.rect.width
 		self.rect.y = self.rect.height
-		
+
 		#Accurate position of the alien
 		self.x = float(self.rect.x)
-		
+
+	def update(self):
+		self.x += (self.game_settings.aliens_speed *
+			self.game_settings.aliens_direction)
+		self.rect.x = self.x
+
+
 	def blitme(self):
 		self.screen.blit(self.image, self.rect)
+
+	def check_edge(self):
+		screen_rect = self.screen.get_rect()
--- alien_game/main.py
+++ alien_game/main.py
@@ -9,33 +9,36 @@ from settings import Settings
 from ship import Ship
 #from alien import Alien
 
+import star_field as sf
+
 def run_game():
+	game_settings = Settings()
+	clock = pygame.time.Clock()
+	WHITE = (255, 255, 255)
+	star_field = sf.background()
+
 	#Инициализация игры
 	pygame.init()
-	game_settings = Settings()
 	screen = pygame.display.set_mode(
 		(game_settings.screen_width, game_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	
+
 	#Создадим корабль
 	ship = Ship(game_settings, screen)
-	
 	#Create an alien
 	#alien = Alien(game_settings, screen)
-	
 	#Bullet Group
 	bullets = Group()
-	
 	#Alliens fleet
 	aliens = Group()
-	
-	gf.create_fleet(game_settings, screen, aliens)
-
+	gf.create_fleet(game_settings, screen, aliens, ship)
 	# Запуск основного цикла игры
 	while True:
-		# Отслеживание событий 
+		# Отслеживание событий
 		gf.check_events(game_settings, screen, ship, bullets)
 		ship.update()
-		gf.update_bullets(bullets)
-		gf.update_screen(game_settings, screen, ship, aliens, bullets)
+		gf.update_bullets(aliens, bullets)
+		gf.update_aliens(aliens, game_settings)
+		gf.update_screen(game_settings, screen, ship, aliens, bullets, star_field, clock, WHITE)
+		clock.tick(60)
 run_game()
